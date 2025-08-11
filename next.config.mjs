c7d2c6ddee5fde6e309fbea9ae59bdf542fbeb2f
Clint <81433235+ryjin111@users.noreply.github.com>
2025-08-11 23:04:44 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'wrpcd.net' },
+      { protocol: 'https', hostname: 'proxy.wrpcd.net' },
+      { protocol: 'https', hostname: 'wallet.farcaster.xyz' },
+      { protocol: 'https', hostname: 'monad-farmer.vercel.app' },
+    ],
+  },
   async headers() {
     return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Origin, X-Requested-With, Content-Type, Accept' },
+        ],
+      },
       {
         source: '/.well-known/farcaster.json',
         headers: [
